f0da9429ef2dc040497f3f4b135a73eeeab19be5
prateikkumarpal <145900568+prateikkumarpal@users.noreply.github.com>
2023-09-24 15:00:45 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main 
+{
+int h,area,vol;
+printf("enter dimenistion");
+scanf("%d",&h);
+area=(2*3.14*r*r + 2*3.14*r*h)
+vol=(3.14*r*r*h)
+printf("area is=%d",area);
+printf("vol=%d",vol);
+}
